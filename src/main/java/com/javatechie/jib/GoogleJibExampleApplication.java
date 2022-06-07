1c706080ac17c4147f772b2f8cfdb5d88cef778b
Subham Mohanty <submohan4@publicisgroupe.net>
2022-06-07 12:09:01 +0530

    first commit

---
+++ src/main/java/com/javatechie/jib/GoogleJibExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.jib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleJibExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleJibExampleApplication.class, args);
+	}
+
+}
